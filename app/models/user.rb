0d7ffefb73b0bb4861b545ba9095223d20eb5810
Surendra <surendra.konna@gmail.com>
2019-07-30 00:33:22 +0530

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  has_many :articles
+
+  validates :email, presence: true, email: true, uniqueness: true
+  validates :password, confirmation: true, length: { minimum: 6  }, on: :create
+
+  before_create :generate_authentication_token
+
+  def generate_authentication_token
+    self.authentication_token = SecureRandom.hex(16)
+  end
+end
