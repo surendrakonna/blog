c8592d829b1a6578cb5a0b861fd29980838cf22b
Surendra <surendra.konna@gmail.com>
2020-03-14 22:50:50 +0530

    user crud apis

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,9 +1,30 @@
 class Api::UsersController < ApplicationController
-  skip_before_action :authenticate_user!, only: :create
+  skip_before_action :authenticate_user!, only: [:create, :show]
 
   def create
     perform User::CreateAction do
       return render json: @action.record
+    end
+    render_action_error @action
+  end
+
+  def show
+    perform User::ShowAction do
+      return render json: @action.record
+    end
+    render_action_error @action
+  end
+
+  def update
+    perform User::UpdateAction do
+      return render json: @action.record
+    end
+    render_action_error @action
+  end
+
+  def destroy
+    perform User::DestroyAction do
+      return render json: { success: true }
     end
     render_action_error @action
   end
--- spec/acceptance/users_spec.rb
+++ spec/acceptance/users_spec.rb
@@ -21,6 +21,103 @@ resource 'Users' do
           expect(status).to eq(200)
         end
       end
+
+      context 'when failed' do
+        let(:params) { { user: { name: 'Surendra', email: ''} } }
+        example '422' do
+          # Act
+          do_request
+
+          # Assert
+          expect(status).to eq(422)
+        end
+      end
+    end
+  end
+
+  route "/api/users/:id", "Get User" do
+    header 'X-ACCESS-TOKEN', :access_token
+    parameter :id, "User ID", type: :integer, example: '123', required: true
+
+    get 'Get User' do
+      context 'when success' do
+        let(:id) { FactoryBot.create(:user).id}
+        # let(:params) { { user: { name: 'Surendra', email: 'user@test.com', password: 'Apple123', password_confirmation: 'Apple123'} } }
+        example '200' do
+          # Act
+          do_request
+
+          # Assert
+          expect(status).to eq(200)
+        end
+      end
+    end
+  end
+
+  route "/api/users/:id", "Update User" do
+    header 'X-ACCESS-TOKEN', :access_token
+    parameter :id, 'User id', type: :integer, example: 100, required: true
+
+    let(:id) { user.id }
+    let(:access_token) { user.authentication_token }
+    let(:user) { FactoryBot.create(:user) }
+
+    put 'Update User' do
+      let(:raw_post) { params.to_json }
+      context do
+        let(:params) { { user: { name: 'Surendra K' } } }
+        example '200' do
+          # Act
+          do_request
+
+          # Assert
+          expect(status).to eq(200)
+          expect(user.reload.name).not_to be_nil
+        end
+      end
+
+      context do
+        let(:params) { { user: { email: '' } } }
+        example '422' do
+          # Act
+          do_request
+
+          # Assert
+          expect(status).to eq(422)
+        end
+      end
+    end
+  end
+
+  route "/api/users/:id", "Delete User" do
+    header 'X-ACCESS-TOKEN', :access_token
+    parameter :id, 'User id', type: :integer, example: 100, required: true
+
+    let(:access_token) { user.authentication_token }
+    let(:user) { FactoryBot.create(:user) }
+
+    delete 'Delete User' do
+      context do
+        let(:id) { user.id }
+        example '200' do
+          # Act
+          do_request
+
+          # Assert
+          expect(status).to eq(200)
+        end
+      end
+
+      context do
+        let(:id) { 0 }
+        example '404' do
+          # Act
+          do_request
+
+          # Assert
+          expect(status).to eq(404)
+        end
+      end
     end
   end
 
