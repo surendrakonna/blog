dbab134ae3b994b57b0e937e3301225f8fb6b70a
Surendra <surendra.konna@gmail.com>
2020-04-17 20:02:39 +0530

    use separate configs for dev and master

--- app/controllers/api/docs_controller.rb
+++ app/controllers/api/docs_controller.rb
@@ -2,7 +2,7 @@
   skip_before_action :authenticate_user!
 
   def index
-    @html = Aws::S3::Object.new(ENV['API_DOCS_BUCKET'], "output.html").get.body.string
+    @html = Aws::S3::Object.new(ENV['API_DOCS_BUCKET'], "api_doc.html").get.body.string
     render html: @html.html_safe, content_type: "text/html"
   end
 
